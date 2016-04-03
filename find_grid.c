bdf2b9265ab155d30d7805f38ce93c422ad3fb01
ipritish <pritishjain2009@gmail.com>
2016-04-03 11:36:26 +0530

    find

---
+++ find_grid.c
@@ -0,0 +1,35 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int t; 
+    scanf("%d",&t);
+    for(int a0 = 0; a0 < t; a0++){
+        int R; 
+        int C; 
+        scanf("%d %d",&R,&C);
+        char* G[R];
+        for(int G_i = 0; G_i < R; G_i++){
+           G[G_i] = (char *)malloc(1024 * sizeof(char));
+           scanf("%s",G[G_i]);
+        }
+        int r; 
+        int c; 
+        scanf("%d %d",&r,&c);
+        char* P[r];
+        for(int P_i = 0; P_i < r; P_i++){
+           P[P_i] = (char *)malloc(1024 * sizeof(char));
+           scanf("%s",P[P_i]);
+        }
+        
+        for(int G_i = 0; G_i < R; G_i++){
+           printf("%s\n",G[G_i]);
+        }
+    }
+    return 0;
+}
