5a688e49ff5f5e5b89cbaed74d4c50b73cd45d41
ipritish <pritishjain2009@gmail.com>
2016-05-06 20:26:25 +0530

    test

--- node_practice.c
+++ node_practice.c
@@ -22,11 +22,31 @@ void traverse_node (struct node_entry *head){
     printf("%d\n",head->entry);
 }
 
+int doesExist(struct node_entry *head, int number){
+   int exists = -1;
+   while(head->next != NULL){
+	if (head->entry == number){
+	    exists = 1;
+	    break;
+	}
+	head = head->next;
+   }
+   return exists;
+}
+
 int main()
 {
     struct node_entry head = create_node(0);
     struct node_entry first = create_node(1);
+    struct node_entry second = create_node(2);
     head.next = &first;
+    first.next = &second;
     traverse_node(&head);
+    if (doesExist(&head,5) == 1 ){
+        printf("Exists\n");
+    }
+    else{
+	printf("Does not Exist\n");
+    }
     return 0;
 }
